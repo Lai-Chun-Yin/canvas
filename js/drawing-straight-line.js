14babc8ddbd233faf8ac7a0357571effb13ba671
cylai0723@gmail.com <cylai0723@gmail.com>
2018-05-29 14:25:04 +0800

    Complete draw straight line

--- js/drawing-straight-line.js
+++ js/drawing-straight-line.js
@@ -1,51 +1,48 @@
-class DrawingStraightLine extends PaintFunction{
-    constructor(contextReal,contextDraft){
+class DrawingStraightLine extends PaintFunction {
+    constructor(contextReal, contextDraft) {
         super();
         this.contextReal = contextReal;
-        this.contextDraft = contextDraft;            
+        this.contextDraft = contextDraft;
     }
-    
-    onMouseDown(coord,event){
-        this.contextReal.strokeStyle = "black";
+
+    onMouseDown(coord, event) {
         this.contextReal.lineJoin = "round";
-        this.contextReal.lineWidth = 5;
-        this.contextDraft.strokeStyle = "black";
         this.contextDraft.lineJoin = "round";
-        this.contextDraft.lineWidth = 5;
         this.contextReal.beginPath();
-        this.contextReal.moveTo(coord[0],coord[1]);
-        this.contextDraft.beginPath();
-        this.contextDraft.moveTo(coord[0],coord[1]);
+        this.contextReal.moveTo(coord[0], coord[1]);
         this.origX = coord[0];
         this.origY = coord[1];
     }
 
-    onDragging(coord,event){
-        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.drawDraft(coord[0],coord[1]);
+    onDragging(coord, event) {
+        this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+        this.contextDraft.beginPath();
+        this.contextDraft.moveTo(this.origX, this.origY);
+        this.drawDraft(coord[0], coord[1]);
     }
 
-    onMouseMove(){}
-    
-    onMouseUp(coord){
-        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.drawReal(coord[0],coord[1]);
-    }
-    onMouseLeave(){}
-    onMouseEnter(){}
+    onMouseMove() { }
 
-    drawDraft(x,y){
-        this.contextDraft.lineTo(x,y);
-        // this.contextDraft.moveTo(x,y);
-        this.contextDraft.closePath();
+    onMouseUp(coord) {
+        this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+        this.drawReal(coord[0], coord[1]);
+    }
+    onMouseLeave() { }
+    onMouseEnter() { }
+
+    drawDraft(x, y) {
+        this.contextDraft.lineTo(x, y);
+        //this.contextDraft.moveTo(x,y);
         this.contextDraft.stroke();
+        this.contextDraft.closePath();
     }
 
-    drawReal(x,y){
-        this.contextReal.lineTo(x,y);
-        this.contextReal.moveTo(x,y);
+    drawReal(x, y) {
+        this.contextReal.lineTo(x, y);
+        //this.contextReal.moveTo(x, y);
+        this.contextReal.stroke();
         this.contextReal.closePath();
-        this.contextReal.stroke();    
+        
     }
 
 }
